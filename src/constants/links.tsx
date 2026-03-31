ca44fe4a91568d9a23a2c3893f61cc907dbc000c
devank <adreshsingh275127@gmail.com>
2026-03-31 13:43:56 +0530

    some modification

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -4,8 +4,9 @@ export const assessmentLinks = [
     { name: "Placement Team - Prompt Test", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-090a13nsb2&invb=usery45kf7tb" },
     { name: "Video Editing Team - Prompt Test", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-4gknloc92i&invb=usery45kf7tb" },
     { name: "Content Team - Prompt Testing -", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-g9iqelfcal&invb=usery45kf7tb" },
-    { name: "Operations Team - Prompting Test", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-u7njippk3p&invb=usery45kf7tb" },
+    { name: "Auditing Team - Prompting Test", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-u7njippk3p&invb=usery45kf7tb" },
     { name: "B2I Team - Prompting Test - Inte", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-zjvlo0xqh7&invb=usery45kf7tb" },
     { name: "Assessment Team - Prompting Test", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-ihx9kmm8jv&invb=usery45kf7tb" },
-    { name: "Sales Team - Prompt Test", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-tledj1zddh&invb=userjzsl6u3e" }
+    { name: "Sales Team - Prompt Test", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-tledj1zddh&invb=userjzsl6u3e" },
+    { name: "Program Successor & Influencer Team - Prompting Test", link: "https://coderbyte.com/sl-candidate?promo=adda247-cktrp:technical-assessment-3hrb4c8ze6&invb=userjzsl6u3e" }
 ];
